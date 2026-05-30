95f152ebd2663adfae6b6d183f979012ee8878fb
harshit <harshit.3851@gmail.com>
2026-05-30 21:10:05 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,12 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var _ = fmt.Print
 
-func handleEcho(s string) {
-	fmt.Println(s)
+func handleEcho(s []string) {
+	for i := 0; i < len(s); i++ {
+		if i != len(s)-1 {
+			fmt.Printf("%s ", s[i])
+		} else {
+			fmt.Printf("%s\n", s[i])
+		}
+	}
+}
+
+func handleType(s string) {
+	typesList := []string{"echo", "exit", "type"}
+
+	for i := 0; i < len(typesList); i++ {
+		if s == typesList[i] {
+			fmt.Printf("%s is a shell builtin\n", s)
+			return
+		}
+	}
+
+	fmt.Printf("%s: not found\n", s)
+}
+
+func getCommandsList(cmd string) []string {
+	return strings.Split(cmd, " ")
 }
 
 func main() {
@@ -25,15 +49,20 @@ func main() {
 		}
 
 		cmd = cmd[0 : len(cmd)-1]
+		cmdList := getCommandsList(cmd)
 
-		if cmd == "exit" {
+		if cmdList[0] == "type" {
+			handleType(cmdList[1])
+			continue
+		}
+
+		if cmdList[0] == "exit" {
 			status = 0
 			break
 		}
 
-		// echo -
-		if cmd[0:5] == "echo " {
-			handleEcho(cmd[5:])
+		if cmdList[0] == "echo" {
+			handleEcho(cmdList[1:])
 			continue
 		}
 
