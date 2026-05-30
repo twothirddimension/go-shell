21445ed37885834b2bf81746077816259dfc0c49
harshit <harshit.3851@gmail.com>
2026-05-30 20:42:08 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 var _ = fmt.Print
 
+func handleEcho(s string) {
+	fmt.Println(s)
+}
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	status := 1
 	for status == 1 {
-		var cmd string
 		fmt.Print("$ ")
-		fmt.Scanln(&cmd)
+
+		cmd, err := reader.ReadString('\n')
+
+		if err != nil {
+			panic(err)
+		}
+
+		cmd = cmd[0 : len(cmd)-1]
 
 		if cmd == "exit" {
 			status = 0
 			break
+		}
+
+		// echo -
+		if cmd[0:5] == "echo " {
+			handleEcho(cmd[5:])
+			continue
 		}
 
 		fmt.Printf("%s: command not found\n", cmd)
